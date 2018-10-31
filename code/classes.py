d030db94babf9a575dbb4453f1089e728a3026a6
mauriciogm97 <mauricio.gm@outlook.com>
2018-10-31 12:12:21 -0600

    created user class

---
+++ code/classes.py
@@ -0,0 +1,10 @@
+class User:
+
+	def __init__(self, name, username, email):
+		self.__name = name
+		self.__username = username
+		self.__email = email
+
+	def addFolder():
+		self.folders.append()
+
--- uiControl.py
+++ uiControl.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from code.classes.py import User
 app = Flask(__name__)
 
 @app.route('/')
